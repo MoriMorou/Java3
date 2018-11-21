c8509048df4d7bba8c2b40ebb6d6dde58cd9382a
MoriMorou <Mori_morou@rocketmail.com>
2018-11-22 01:22:47 +0300

    New gitignore

--- Examples/src/test/java/ru/morimorou/java/Reflection_API_and_Annotations/Ex1_TestReflection/TestReflection.java
+++ Examples/src/test/java/ru/morimorou/java/Reflection_API_and_Annotations/Ex1_TestReflection/TestReflection.java
@@ -1,4 +1,24 @@
 package ru.morimorou.java.Reflection_API_and_Annotations.Ex1_TestReflection;
 
+import com.sun.istack.internal.Nullable;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestReflection {
+
+//    @NotEmpty
+//    public String value;
+//
+//
+//    @Nullable
+//    public String welcome;
+//
+//    @Test
+//    public void testClass(){
+//        Assert.assertEquals(TestReflection.class, this.getClass());
+//        System.out.println(this.getClass().getPackage().getName());
+//        System.out.println(this.getClass().getName());
+//        System.out.println(this.getClass().getSimpleName());
+//    }
+
 }
